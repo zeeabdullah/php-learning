8ed1fd5a7618620812690fdbe423376df0ced710
zeeabdullah <zeeabdullah1419@gmail.com>
2022-06-14 03:04:16 +0300

    upload 1

---
+++ phptest.php
@@ -0,0 +1,18 @@
+<?php
+// string and int
+$fname = 'Zahra';
+$sname= 'Abdullah';
+$x= 6;
+$y= 9;
+
+$name = $fname . ' ' . $sname;
+$z= $x * $y ;
+
+echo $name . '<br />' ;
+echo $z;
+
+// array
+
+$letters = ['A', 'B', 'C'];
+echo $letters[2];
+print_r($letters);
